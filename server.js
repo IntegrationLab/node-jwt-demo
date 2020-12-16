c87476988e890702a33a401c25175d6e5d7962d9
Jaise Thomas <jaise.thomas@aot-technologies.com>
2020-12-16 01:18:22 -0800

    load env + test requests for login

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 const express = require('express')
 const app = express()
 
@@ -33,7 +35,10 @@ app.post('/login',(req,res) =>{
     const username = req.body.username
     const user = {name:username}
 
-    jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
+    const accessToken =   jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
+
+    res.json({accessToken: accessToken})
+
 
 })
 
