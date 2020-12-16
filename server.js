d0aaa652a6bbd7b7c4fb5d24e9c9edaadcc36b4b
Jaise Thomas <jaise.thomas@aot-technologies.com>
2020-12-16 00:48:00 -0800

    sample book collection and test requests

--- server.js
+++ server.js
@@ -1,4 +1,25 @@
 const express = require('express')
 const app = express()
 
+const books = [
+    {
+        author: "Steve Berry",
+        title: "Bishop's Pawn"
+    },
+    {
+        author: "Arundhathi Roy",
+        title: "The God of Small Things"
+    },
+    {
+        author: "Harper Lee",
+        title: "To Kill A Mockingbird"
+    }
+]
+
+app.get('/books', (req,res) =>{
+
+    res.json(books)
+
+})
+
 app.listen(3000)
